53b3758cead740300cf43ab22162f6bb3795bc06
GGu <ggu@talend.com>
2014-02-18 15:19:45 +0800

    Add a shell script and updat the svn list

---
+++ studio/branch_diff/list_SVN_Repo.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+svnUser=$1
+svnPass=$2
+
+#Check the input arguments
+if [ $# -lt 2 ] ;then
+cat<<EOF
+============================ Usage: ============================ 
+=                                                              =
+=  ./list_SVN_Repo.sh <svnUser> <svnPass>                      =
+=                                                              =
+================================================================ 
+EOF
+    exit
+fi
+
+baseFolder="../migration_data/branches"
+for branches in 5_0 5_1 5_2 5_3 5_4
+do
+	branchFolder="$baseFolder/branch-$branches/svn"
+	mkdir -p $branchFolder
+	for repo in tos top tom tis_shared tis_private tdq tem 
+	do 
+		echo "$branches $repo" 
+		repoFile=$branchFolder/$repo.$branches
+		svn list --depth immediates http://talendforge.org/svn/$repo/branches/branch-$branches/ --username $svnUser --password $svnPass > $repoFile
+		sort $repoFile > $repoFile.sorted
+		rm -rf $repoFile
+	done
+done
+
+#trunk(master)
+branchFolder="$baseFolder/master/svn"
+mkdir -p $branchFolder
+for repo in tos top tom tis_shared tis_private tdq tem 
+do 
+	echo "trunk $repo" 
+	repoFile=$branchFolder/$repo.trunk
+	svn list --depth immediates http://talendforge.org/svn/$repo/trunk/  --username $svnUser --password $svnPass >  $repoFile
+	sort $repoFile > $repoFile.sorted
+	rm -rf $repoFile
+done
