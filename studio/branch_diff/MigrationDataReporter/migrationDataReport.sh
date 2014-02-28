ed7c62d3160af235080afe8b0da3135020d1d2de
GGu <ggu@talend.com>
2014-02-28 11:29:53 +0800

    update the migration data report

--- studio/branch_diff/MigrationDataReporter/migrationDataReport.sh
+++ studio/branch_diff/MigrationDataReporter/migrationDataReport.sh
@@ -2,4 +2,7 @@
 baseFolder="../../migration_data/branches"
 reporterFile="MigrationData.report"
 
+# Support filter repository, using "-Dfilter.values=tom,tem"
+# Also, can compare the migration data between too branch, using "-Denable.compare.data=true"
+
 java -cp MigrationDataReporter.jar org.talend.svn2git.migration.data.MigrationDataReporter $baseFolder > "$baseFolder/$reporterFile"
