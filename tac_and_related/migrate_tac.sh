ff65c34a78d8e5b9ff5cb035d7822989b8a5d69c
Stéphane Mallet <smallet@talend.com>
2014-02-28 15:25:32 +0100

    Add jobserver capabilities

--- tac_and_related/migrate_tac.sh
+++ tac_and_related/migrate_tac.sh
@@ -11,13 +11,7 @@ clone () {
   echo ' ---> Clean branches & tags with svn-migration-scripts.jar'
   time java -Dfile.encoding=utf-8 -jar ~/talend-svn-git-migration/svn-migration-scripts.jar clean-git --force
 
-  echo ' ---> Delete some branches'
-  for branch in branch-2_1 branch-2_2 branch-2_3 branch-2_4 branch-3_0 branch-3_1 branch-3_2 branch-4_0 branch-4_1 branch-4_1_metadata branch-splitmodel branch-tempbuild-5_4_0 jobserver-karaf branch-3_2_mdm mhelleboid-1 mhelleboid-2 branch-1_0_gxt TOD-prod;
-  do
-    git branch -D $branch
-  done 
-
-  to_remove=`ls ~/checkout/$1 --hide pom.xml | grep -f ~/talend-svn-git-migration/tac_and_related/motif_$1 -xv`
+  to_remove=`ls ~/checkout/$1 --hide pom.xml | grep -f ~/talend-svn-git-migration/tac_and_related/$1To$2 -xv`
   for folder in $to_remove;
   do
     echo ------------------------------------
@@ -34,6 +28,7 @@ clone () {
 }
 
 merge () {
+  cd ~/checkout/merge/
   git remote add $1 ../$1
   git fetch $1
   for branch in master branch-5_4 branch-5_3 branch-5_2 branch-5_1 branch-5_0 branch-4_2;
@@ -41,11 +36,11 @@ merge () {
     git checkout $branch
     git merge $1/$branch -m "GIT admin: merge $1/$branch projects in TAC"
   done
-  git remote add $1 ../$1 --tags
+  #git remote add $1 ../$1 --tags
 }
 
 prepare_merge () {
-  cd
+  cd ~/checkout/
   rm -rf merge
   mkdir merge
   cd merge
@@ -61,10 +56,10 @@ prepare_merge () {
   git branch branch-4_2
 }
 
-clone tis_private
-#clone tis_shared
-#prepare_merge
-#merge tis_shared
+clone tis_private JobServer
+clone tis_shared JobServer
+prepare_merge
+merge tis_shared
 #merge tis_private
 
 #git remote add origin ssh://git@bitbucket.org/talend/tac.git
