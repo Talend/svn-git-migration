b58a463595ac52a75ab529bb8976c49a4df00c89
GGu <ggu@talend.com>
2014-05-28 10:11:47 +0800

    move the local patches script to post folder.

--- studio/migration_data/post_migration_scripts/doApplyLocalPatches.sh
+++ studio/migration_data/post_migration_scripts/doApplyLocalPatches.sh
@@ -3,20 +3,23 @@
 finalReposPath=$1
 
 if [ $# -lt 1 ]; then
-	echo "Must provide the final repositories path (absolute)."
-	exit 1
+	#same as migrate testutils.sh to set the default
+	finalReposPath=$(cd "../final_repos"; pwd) 
 fi
-
-cd "$(dirname $0)"
 
 #if exited, delete the old one
 finalPathesPath="$finalReposPath/_patches_"
 [ -d $finalPathesPath ] && rm -rf $finalPathesPath
 
 #copy all patches file to final repository folder
-cp -rf apply_patches/_patches_  $finalReposPath
+cp -rf local_svn_patch/apply_patches/_patches_  $finalReposPath
 
 cd "$finalReposPath"
+
 /bin/bash $finalReposPath/_patches_/checkTags.sh $finalReposPath
 /bin/bash $finalReposPath/_patches_/applyBranchesPatch.sh
+
+#need delete the tmp patches files?
+#rm -rf $finalReposPath
+
 
